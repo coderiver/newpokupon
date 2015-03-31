3610f6b8808cf468f6e8d08f6df46f5c08563e5b
moroz <arturmorozv@gmail.com>
2015-03-31 13:09:34 +0300

    update slider

--- js/common.js
+++ js/common.js
@@ -4,25 +4,46 @@ head.ready(function() {
 (function () {
     var slEl = $('.js-sl');
     if (slEl.length) {
-    	var slNav = $('.js-sl-nav');
-    	slEl.slick({
-    		slidesToShow: 1,
-    		slidesToScroll: 1,
-    		asNavFor: slNav
-    	});
-    	slNav.slick({
- 			slidesToShow: 6,
-			slidesToScroll: 1,
-			arrows: false,
-			swipe: false,
-			asNavFor: slEl,
-			centerMode: true,
-			focusOnSelect: true
-    	});
+        var slNav = $('.js-sl-nav');
+        if (slEl.find('>div').length > 6) {
+            slEl.slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                asNavFor: slNav
+            });
+            slNav.slick({
+                slidesToShow: 6,
+                slidesToScroll: 1,
+                arrows: false,
+                swipe: false,
+                asNavFor: slEl,
+                centerMode: true,
+                focusOnSelect: true
+            });
+        }
+    	else {
+            slNav.addClass('is-six');
+            slEl.slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                asNavFor: slNav
+            });
+            slNav.slick({
+                slidesToShow: 6,
+                slidesToScroll: 1,
+                arrows: false,
+                swipe: false,
+                asNavFor: slEl,
+                focusOnSelect: true
+            });
+            slNav.find('.slick-slide').eq(0).addClass('is-active');
+            slEl.on('afterChange', function(event, slick, currentSlide, nextSlide){
+                slNavSlide = slNav.find('.slick-slide');
+                slNavSlide.removeClass('is-active');
+                slNavSlide.eq(currentSlide).addClass('is-active');
+            });
+        }
     };
  }());
-
-
-	
 
 });
